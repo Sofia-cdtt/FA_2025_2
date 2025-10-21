6a91d35e023fe958bccf276f746f8f92bc580c98
Sofia <SofiaCandiottiS@gmail.com>
2025-10-21 15:49:32 -0500

    Subiendo ejercicio semana 10

---
+++ UNIDAD 2/SEMANA10_CSHARD/Biblioteca/Cajero.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Cajero
+    {
+        double saldo = 1000;
+
+        public double Consultar()
+        {
+            return saldo;
+        }
+
+        public void Depositar(double monto) 
+        {
+            while (true)
+            {
+
+                Console.Write("\nIngrese el monto a depositar: ");
+                string en = Console.ReadLine();
+            
+                try
+                {
+                    monto = Convert.ToDouble(en);
+
+                    if (monto > 0)
+                    {
+                        saldo += monto;
+                        Console.WriteLine("Deposito exitoso.");
+                        break;
+                    }
+
+                    else 
+                    {
+                        Console.WriteLine("ERROR. Tiene que ser un deposito mayor a cero. ");
+                        continue; //no lo considera y vuelve a ejecutar el ingreso del monto a depositar
+                    }
+                        
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("ERROR. El deposito tiene que ser un monto válido. ");
+                    continue;
+                }
+            }
+            
+        }
+
+        public void Retirar(double monto)
+        {
+            while (true)
+            {
+
+                Console.Write("\nIngrese el monto a retirar: ");
+                string en = Console.ReadLine();
+
+                try
+                {
+                    monto = Convert.ToDouble(en);
+
+                    if (monto <= saldo)
+                    {
+                        saldo -= monto;
+                        Console.WriteLine("Retiro exitoso.");
+                        break;
+                    }
+                    else if (monto < 0) 
+                    {
+                        Console.WriteLine("ERROR. Tiene que ser un retiro mayor a cero.\n");
+                        continue;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("ERROR. Saldo insuficiente\n. ");
+                        continue; //no lo considera y vuelve a ejecutar el ingreso del monto a depositar
+                    }
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("ERROR. El retiro tiene que ser un monto válido. ");
+                    continue;
+                }
+            }
+
+        }
+    }
+}
---
+++ UNIDAD 2/SEMANA10_CSHARD/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using Biblioteca;
+
+namespace SEMANA10_CSHARD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Cajero c = new Cajero();
+                int opc;
+                string seguir;
+
+            do
+            {
+
+                Console.WriteLine("BIENVENIDO AL SISTEMA DE CAJERO\n");
+                Console.WriteLine("1. Consultar saldo. ");
+                Console.WriteLine("2. Depositar dinero. ");
+                Console.WriteLine("3. Retirar dinero. ");
+                Console.WriteLine("4. Salir. ");
+
+                do
+                {
+                    Console.Write("Ingrese una opción: ");
+                    opc = int.Parse(Console.ReadLine());
+
+                    if (opc < 0 | opc > 4)
+                    {
+                        Console.WriteLine("Opción no válida. Ingrese nuevamente\n");
+                    }
+
+                } while (opc < 0 | opc > 4); // puede ser || o |
+
+                switch (opc)
+                {
+                    case 1: Console.WriteLine("\nSaldo disponible: " + c.Consultar()); break;
+                    case 2: c.Depositar(0); break;
+                    case 3: c.Retirar(0); break;
+                    case 4: return;
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("\n¿Desea continuar? (s/n): ");
+                    seguir = Console.ReadLine().ToUpper();
+
+                    if (seguir != "S" & seguir != "N")
+                    {
+                        Console.WriteLine("ERROR. Solo permiten 's' o 'n'. ");
+                    }
+                    else break;
+                }
+                Console.Clear();
+            } 
+            while (seguir == "S");
+        }
+    }
+}

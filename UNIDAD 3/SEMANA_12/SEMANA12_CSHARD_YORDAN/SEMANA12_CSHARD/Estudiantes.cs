8b05f6e65e0696c64b56ca7e1861971bd81ceba5
Sofia <SofiaCandiottiS@gmail.com>
2025-11-04 15:59:16 -0500

    Subiendo ejercicios del profesor Yordan

---
+++ UNIDAD 3/SEMANA_12/SEMANA12_CSHARD_YORDAN/SEMANA12_CSHARD/Estudiantes.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA12_CSHARD
+{
+    internal class Estudiantes
+    {
+        // Estas variables siempre debe ser global
+        // para que puedas usarlas en otros métodos
+        byte[] edades = new byte[0]; //para optimizar recursos
+        int pos = 0;
+
+        public void Registrar() 
+        {
+            byte edad;
+            
+            while (true)
+            {
+                Console.Write("\nIngrese edad: ");
+                if (byte.TryParse(Console.ReadLine(), out edad) & edad >= 14 && edad <= 120) break; 
+                else Console.WriteLine("¡Ingrese una edad correcta!\n");
+            }
+
+            Array.Resize(ref edades, edades.Length + 1);
+            edades[pos] = edad;
+            pos++;
+
+            Console.WriteLine("\nEdad registrada correctamente. ");
+        }
+
+        public void Mostrar() 
+        {
+            Console.WriteLine("\nPOS\tEDAD");
+            
+            for (int i = 0; i < edades.Length; i++) 
+            {
+                Console.WriteLine(i + "\t" + edades[i]);
+            }
+        }
+
+        public void Eliminar() 
+        {
+            int Índice = -1;
+            Console.Write("\nIngrese la edad a eliminar: ");
+            byte edad = byte.Parse(Console.ReadLine());
+
+            for (int i = 0; i < edades.Length; i++) 
+            {
+                if (edades[i] == edad)
+                    Índice = i;
+            }
+
+            if (Índice != -1)
+            {
+                for (int j = Índice; j < edades.Length-1; j++)
+                    edades[j] = edades[j + 1];
+
+                Array.Resize(ref edades, edades.Length - 1);
+                pos--;
+                Console.WriteLine("\nEdad eliminada correctamente");
+            }
+            else Console.WriteLine("\nNo se puede eliminar porque no existe.");
+        }
+
+        public void Ordenar() 
+        {
+            for (int i = 0; i < edades.Length - 1; i++) 
+            {
+                for (int j = 0; j < edades.Length - 1 - i; j++) 
+                {
+                    if (edades[j] < edades[j + 1]) 
+                    {
+                        byte temp = edades[j];
+                        edades[j] = edades[j + 1];
+                        edades[j+1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("\nSe ordeno correctamente. ");
+        }
+    }
+}
---
+++ UNIDAD 3/SEMANA_12/SEMANA12_CSHARD_YORDAN/SEMANA12_CSHARD/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA12_CSHARD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string continuar;
+            Estudiantes e = new Estudiantes();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("BIENVENIDOS AL SISTEMA DE REGISTRO DE EDADES\n");
+                Console.WriteLine("Menú de opciones: \n");
+                Console.WriteLine(" 1. Registrar ");
+                Console.WriteLine(" 2. Mostrar ");
+                Console.WriteLine(" 3. Eliminar ");
+                Console.WriteLine(" 4. Ordenar ");
+                Console.WriteLine(" 0. Salir\n ");
+
+                int opc;
+
+
+                while (true)
+                {
+                    Console.Write("Ingrese una opción: ");
+                    if (int.TryParse(Console.ReadLine(), out opc) & opc >= 0 && opc <= 4) break; //si la conversion es correcta sal y agregaló a esta variable opc
+                    else Console.WriteLine("¡Ingrese una opción correcta!\n");
+                }
+
+                switch (opc)
+                {
+                    case 0: return;
+                    case 1: e.Registrar(); break;
+                    case 2: e.Mostrar(); break;
+                    case 3: e.Eliminar(); break;
+                    case 4: e.Ordenar();  break;
+                }
+
+                while (true)
+                {
+                    Console.Write("¿Desea continuar? [S/N] : ");
+                    continuar = Console.ReadLine().ToUpper();
+                    if (continuar == "S" || continuar == "N") break;
+                    else Console.WriteLine("ERROR. Ingrese solo 's' o 'n'\n");
+                }
+                Console.WriteLine("\nGracias por utilizar el sistema. ");
+
+            } while (continuar == "S");
+            
+        }
+    }
+}
